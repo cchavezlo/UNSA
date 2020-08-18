d9dd916d92fb559895f704283c77d11bac743c30
Erika Tamo Turpo <etamotu@gmail.com>
2020-08-18 15:50:54 -0500

    Modificando calcul delta y el tiempo

--- ComputacionMolecularBiologica/Final/final.py
+++ ComputacionMolecularBiologica/Final/final.py
@@ -6,42 +6,8 @@
 import random 
 import pandas as pd
 import numpy as np
-import time
-df = pd.read_csv('C:/Users/ErikaPC/Documents/Cursos2020-1/BioinformaticaMoleculaar/FINAL/x60189_4/matrix_conservative.csv',sep =",",comment="#")
-tamano=len(df)
-#print(tamano)
-w=np.asarray(df)
-#print(w)
-def encontrar_menor(L):
-    # get the posible movement with minimun delta_c
-    x = len(L)
-    L_temp = np.matrix(L)
-    delta_c_list = L_temp[:,3]
-    min_delta_c = np.amin(delta_c_list)
+from timeit import default_timer 
 
-    L_with_min_delta_c = []
-    for i in range(x):
-        if L_temp[i,3] == min_delta_c:
-            L_with_min_delta_c.append(np.squeeze(np.asarray(L_temp[i,:])))
-
-       
-    # get the posible movement with maximun delta_f
-    x = len(L_with_min_delta_c)
-    L_temp = np.matrix(L_with_min_delta_c)
-    delta_f_list = L_temp[:,2]
-    max_delta_f = np.amax(delta_f_list)
-    
-    L_with_max_delta_f = []
-    for i in range(x):
-        if L_temp[i,2] == max_delta_f:
-            L_with_max_delta_f.append(np.squeeze(np.asarray(L_temp[i,:])))
-
-    L_temp = np.matrix(L_with_max_delta_f)
-    
-    #print(L_temp.shape)   
-    #print(L_temp)
-    return int(L_temp[0, 0]), int(L_temp[0, 1])
-    """
 def encontrar_menor(L):
 	global N
 	ordenado=sorted(L,key=lambda x: x[-1:])# de menor a mayor
@@ -51,48 +17,53 @@ def encontrar_menor(L):
 	j=1
 	igual=True
 	print(ordenado[i][-1:])
-	if ordenado[i][-1:]==ordenado[j][-1:]:
+	escogido=ordenado[i][0]
+
+	if tmn>=2 and ordenado[i][-1:]==ordenado[j][-1:]:
 		mejor=ordenado[i]
 		while i<tmn and j<tmn and ordenado[i][-1:]==ordenado[j][-1:]:
-			
 			if ordenado[j][2]>mejor[2]:
 				mejor=ordenado[j]
 			i+=1
 			j+=1
 		print(mejor)
-		return mejor[0],mejor[1],mejor[2]
+		return mejor[0],mejor[1]
 	else:
-		return ordenado[0][0],ordenado[0][1],ordenado[0][2]
-"""
-M=[]
+		return ordenado[0][0],ordenado[0][1]
 
 
-N=tamano
-#################inicializar soluciones#####
-cutoff=30
-s=[]
-sequencia=""
-i=0
-while i<N:
-	alea=random.randint(0,N-1)
-	while alea in s:
+
+
+def generar_inicialSolucion(N):
+	s=[]
+	i=0
+	while i<N:
 		alea=random.randint(0,N-1)
-	s.append(alea)
-	i+=1
+		while alea in s:
+			alea=random.randint(0,N-1)
+		s.append(alea)
+		i+=1
+	return s
 
-#print("Orden de la sequencia aleatorioa \n",s)
+
 
 def calculadelta(s,i,j):
 	global w,N
 	cutoff=30
 	dc=0
 	df=0
-	df=df-w[s[i-1]][s[i]]-w[s[j]][s[j+1]]
-	df=df + w[s[i-1]][s[j]]- w[s[i]][s[j+1]]
+	#df=df-w[s[i-1]][s[i]]-w[s[j]][s[j+1]]
+	#df=w[s[i-1]][s[j]]+w[s[i]][s[j+1]]
+	
+	df=w[s[i-1]][s[j]]+w[s[i]][s[j+1]] #añade el sobre lape de la solucion modificada
+	df=df-w[s[i-1]][s[i]]-w[s[j]][s[j+1]] #remover el sobre los actuales soluciones
+	#df=w[s[i-1]][s[j]]+ w[s[i]][s[j+1]]
+	#df=df - w[s[i-1]][s[i]]- w[s[j]][s[j+1]]
 	if w[s[i-1]][s[i]]>cutoff:
 		dc=dc+1
 	if  w[s[j]][s[j+1]]>cutoff:
 		dc=dc+1
+	#hace test si dos coting son unidos, y si asi , esto disminuye la cantidad de contints
 	if w[s[i-1]][s[j]]>cutoff:
 		dc=dc-1
 	if w[s[i]][s[j+1]]>cutoff:
@@ -100,25 +71,43 @@ def calculadelta(s,i,j):
 	return df,dc
 
 def invierte_posiciones(s1,i,j):
+	print("i=",i)
+	print("j=",j)
+	print("secuencia",s1)
 	global N
 	tmp=s1[i]
 	s1[i]=s1[j]
 	s1[j]=tmp
-	return s
+	print("secuencia",s1)
+	return s1
 
+df = pd.read_csv('C:/Users/Erika/Documents/biomolecular/ComputacionMolecularBiologica/Final/x60189_6_68/matrix_conservative.csv',sep =",",comment="#")
+
+N=len(df)
+print(N)
+w=np.asarray(df)
+print(w)
+s=generar_inicialSolucion(N)
+
+cutoff=30
 acumulado=0
 cambio=True
 df=-1
 canti=0
 temp=0
-while temp<300:
+
+#########PALS###############################
+inicio = default_timer()
+while cambio:
+	print("secuencia inicial",s)
 	L=[]
-	i=1
-	while i<N:
-		j=0
+	i=0
+	while i<N-2:
+		j=i+1
 		while j<N-1:
 			dc,df=calculadelta(s,i,j)
-			if dc<0 or (dc==0 and df>0):
+			#print("dc",dc,"df",df)
+			if (dc < 0) or (dc == 0 and df > 0):#dc>=0:#dc<0 or (dc==0 and df>0):
 				L.append([i,j,df,dc])
 			j+=1
 		i+=1
@@ -129,12 +118,15 @@ while temp<300:
 		#acumulado+=dfitnes
 		canti+=1
 		s=invierte_posiciones(s,i,j)
-
+		print("secuencia final",s)
 	else:
 		cambio=False
 		break
+	
 	temp+=1
-
+fin = default_timer()
+print("Tiempo total obtenido"+str(fin-inicio)+" \n")#calcula el tiempo en segundos
+#########PALS###############################
 #print("s",s)
 #print("esto es df",df)
 
